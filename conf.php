a6aa1d380722402dd09f4f0889adfc2b1d22ee7a
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-07 04:25:33 +0100

    config file
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+// hard coded password the best for the security :)
+// I dontt wan to uise a composser
+
+$super_config = [];
+$super_config['sql']=[];
+$super_config['sql']['user']='user';
+$super_config['sql']['password']='password';
+$super_config['sql']['db']='database';
+$super_config['sql']['host']='host';
+
+
