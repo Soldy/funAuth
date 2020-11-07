14d487e0f8be2d1827c0a3a7e9ffdba60efcd448
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-07 19:55:28 +0000

    remove useless php code
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- views/elements/message.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// Usuly that twig or blade ... or some js (angular/ or jsx)
-
-foreach($messages as &$element)
-    echo ('<div>'.$element.'</div>');
--- web/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-require_once('../src/kernel.php');
-
-
